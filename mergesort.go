835934d80bf1cbf5634a93e5e56a5cac9eb413e8
ww <wei.wang@baishan.com>
2022-05-15 04:05:14 +0800

    init mergesort

---
+++ mergesort.go
@@ -0,0 +1,16 @@
+/*
+ * @Author: ww
+ * @Date: 2022-05-15 04:03:34
+ * @Description:
+ * @FilePath: /sorts/mergesort.go
+ */
+package sorts
+
+type MergeSort struct {
+	arr  []int
+	size int
+}
+
+func (this *MergeSort) Sort(arr []int) {
+
+}
