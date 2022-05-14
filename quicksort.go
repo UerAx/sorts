d387cf7bb70cf51301f29f8920465c4822267b26
ww <wei.wang@baishan.com>
2022-05-15 03:52:36 +0800

    add quick sort

--- quicksort.go
+++ quicksort.go
@@ -1,6 +1,46 @@
 package sorts
 
 type QuickSort struct {
-	arr []int
+	arr  []int
+	size int
+}
+
+func (this *QuickSort) Sort(arr []int) {
+	this.add(arr)
+	this.quickSort(0, this.size - 1)
+}
+
+func (this *QuickSort) add(arr []int) {
+	this.arr = arr
+	this.size = len(arr)
+}
+
+func (this *QuickSort) quickSort(left, right int) {
+	if right <= left {
+		return
+	}
+	
+	key := this.getKey(left, right)
+
+	l,r := left,right
+
+	for(l < r) {
+		for l < r && this.arr[r] >= key {r--}
+		this.arr[l] = this.arr[r]
+		for l < r && this.arr[l] <= key {l++}
+		this.arr[r] = this.arr[l]
+	}
+
+	this.arr[l] = key
+	this.quickSort(left, l-1)
+	this.quickSort(l+1, right)
+	
+
+}
+
+func (this *QuickSort) getKey(left, right int) int {
+	mid := (left+right)>>1
+	this.arr[left], this.arr[mid] = this.arr[mid], this.arr[left]
+	return this.arr[left]
 }
 
--- sort_test.go
+++ sort_test.go
@@ -1,7 +1,7 @@
 /*
  * @Author: UerAx
  * @Date: 2022-05-14 23:32:56
- * @FilePath: \sorts\sort_test.go
+ * @FilePath: /sorts/sort_test.go
  * Copyright (c) 2022 by UerAx uerax@live.com, All Rights Reserved.
  */
 package sorts
@@ -9,9 +9,12 @@ package sorts
 import "testing"
 
 func TestSort(t *testing.T) {
-	var s Sort
-	s = new(HeapSort)
+	s := new(HeapSort)
 	arr := []int{5, 2, 6}
 	s.Sort(arr)
 	t.Log(s)
+	s1 := new(QuickSort)
+	arr1 := []int{}
+	s1.Sort(arr1)
+	t.Logf("%v", s1)
 }
