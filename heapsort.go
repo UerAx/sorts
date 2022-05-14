8fcbd652b0141b746d608ca0da2a29caa8a346a3
UerAx <uerax@live.com>
2022-05-15 02:12:12 +0800

    add heap sort

--- heapsort.go
+++ heapsort.go
@@ -1,45 +1,48 @@
+/*
+ * @Author: UerAx
+ * @Date: 2022-05-14 23:32:56
+ * @FilePath: \sorts\heapsort.go
+ * Copyright (c) 2022 by UerAx uerax@live.com, All Rights Reserved.
+ */
 package sorts
 
-
 type HeapSort struct {
-	arr []int
+	arr  []int
+	size int
 }
 
 func (this *HeapSort) Sort(arr []int) {
 	this.add(arr)
 	this.adjustUp()
-	for i := len(this.arr) - 1; i > 0; i-- {
-		this.arr[0], this.arr[i] = this.arr[i], this.arr[0]
-		this.adjustDown(0, i)
+	for i := this.size - 1; i > 0; i-- {
+		this.arr[i], this.arr[0] = this.arr[0], this.arr[i]
+		this.adjust(0, i)
 	}
 }
 
 func (this *HeapSort) add(arr []int) {
-	this.arr = arr;
+	this.arr = arr
+	this.size = len(arr)
 }
 
 func (this *HeapSort) adjustUp() {
-	for i := len(this.arr) - 1; i > 0; i--{
-		if this.arr[i] > this.arr[(i-1)/2] {
-			this.arr[i], this.arr[(i-1)/2] = this.arr[(i-1)/2], this.arr[i]
-			this.adjustDown(i, len(this.arr) - 1)
-		}
+	for i := (this.size >> 1) - 1; i >= 0; i-- {
+		this.adjust(i, this.size)
 	}
 }
 
-func (this *HeapSort) adjustDown(index, size int) {
-	if index >= size || index * 2 + 1 >= len(this.arr) {
+func (this *HeapSort) adjust(idx, size int) {
+	if idx > size || idx<<1 >= size-1 {
 		return
 	}
-	son := index * 2 + 1
-	if (son > size) {
-		return
-	}
-	if son + 1 < size && son + 1 < len(this.arr) && this.arr[son] < this.arr[son+1] {
+
+	son := (idx << 1) + 1
+	if son+1 < size && this.arr[son] < this.arr[son+1] {
 		son++
 	}
-	if this.arr[son] > this.arr[index] {
-		this.arr[son], this.arr[index] = this.arr[index], this.arr[son]
-		this.adjustDown(son, size)
+
+	if this.arr[son] > this.arr[idx] {
+		this.arr[son], this.arr[idx] = this.arr[idx], this.arr[son]
+		this.adjust(son, size)
 	}
 }
