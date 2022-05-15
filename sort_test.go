28be6273240baed1f72cd39140a4c8d8e02b464f
UerAx <uerax@live.com>
2022-05-15 19:23:05 +0800

    add mergesort

--- sort_test.go
+++ sort_test.go
@@ -1,7 +1,7 @@
 /*
  * @Author: UerAx
  * @Date: 2022-05-14 23:32:56
- * @FilePath: /sorts/sort_test.go
+ * @FilePath: \sorts\sort_test.go
  * Copyright (c) 2022 by UerAx uerax@live.com, All Rights Reserved.
  */
 package sorts
@@ -17,4 +17,9 @@ func TestSort(t *testing.T) {
 	arr1 := []int{}
 	s1.Sort(arr1)
 	t.Logf("%v", s1)
+	s2 := new(MergeSort)
+	arr2 := []int{1, 3, 5, 7, 9, 2, 4, 8, 6, 4}
+	s2.Sort(arr2)
+	t.Logf("%v", s2)
+
 }
